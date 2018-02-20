9b2ff9d3c8d4e7f88109754b77edd208599dcca2
Samantha Hughes <hughes2@law.upenn.edu>
2018-02-19 20:35:11 -0500

    Adding initial Pseudocode to game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+
+//!!START!!
+
+    //cptr chooses random letter a-z (for-loop through 24 letters)
+
+
+    //player guesses a letter ranging from a-z 
+
+
+//!!CORRECT!!
+    //if player guesses correct letter, then "Wins:" increase by 1 (update)
+    //Start game over (do not refresh page)
+
+
+//!!INCORRECT!!
+
+    //if letter is wrong 
+        //# of guesses left decreases by 1 (update)
+        //"guesses so far" are displayed... show what the user has entered (.innerHTML?)
+
+//!!LOSSES
+        //if user guesses 9 times, then "Losses:" increase by 1 (update)
+        //Start game over (do not refresh page)
