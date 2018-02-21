baa54bc10b2f0694f50296e628ab10a38b1ec727
Samantha Hughes <hughes2@law.upenn.edu>
2018-02-21 16:04:45 -0500

    adding the basic html to the website

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,35 @@
 
 //!!START!!
 
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     //cptr chooses random letter a-z (for-loop through 24 letters)
 
 
-    //player guesses a letter ranging from a-z 
+    //player guesses a letter ranging from a-z (onkeyUP)
 
 
 //!!CORRECT!!
     //if player guesses correct letter, then "Wins:" increase by 1 (update)
+
     //Start game over (do not refresh page)
+
+        //cptr chooses new letter
 
 
 //!!INCORRECT!!
 
     //if letter is wrong 
         //# of guesses left decreases by 1 (update)
+
+
         //"guesses so far" are displayed... show what the user has entered (.innerHTML?)
 
 //!!LOSSES
-        //if user guesses 9 times, then "Losses:" increase by 1 (update)
-        //Start game over (do not refresh page)
+    //if user guesses 9 times, then "Losses:" increase by 1 (update)
+
+    //Start game over (do not refresh page)
+
+        //cptr chooses new letter
+
+        
