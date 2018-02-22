8e0ed1ff32d897540aa50e9d1f9b128673e37f6c
Samantha Hughes <hughes2@law.upenn.edu>
2018-02-22 15:20:13 -0500

    adding correct guessesLeft and Losses function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,32 +3,77 @@
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 0;
-var guesses = 0;
-
+var guessesLeft = 11;
+var guesses = [];
 
 
 document.onkeyup = function (event) {
     //player guesses a letter ranging from a-z (onkeyUP)
-    var userGuesses = event.key.toLowerCase();
+    var userGuesses = String.fromCharCode(event.keyCode).toLowerCase();
+
     //cptr chooses random letter a-z 
-    var compChoices = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    var compChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    
+
+    //displaying user's input in console
     console.log("user's guess: " + userGuesses);
+
+    // user has to enter an alphabet character, not any key
+
 
     //computer guess
 
     var computerInput = compChoices[Math.floor(Math.random() * 26)];
 
+    //displaying computer's input in console
     console.log("computer input: " + computerInput);
 
+    // document.getElementById("guessesLeft").innerHTML = guessesLeft;
+
+    var updateGuessesLeft = function () {
+        document.querySelector('#guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
         //!!CORRECT!!
-    //if player guesses correct letter, then "Wins:" increase by 1 (update)
+    };
+
+    //if computerInput = userInput, then "Wins:" increases by 1 (update)
 
     if (computerInput === userGuesses) {
         wins++;
         document.getElementById("wins").textContent = wins;
+        guesses.length = 0;
+    }
+    //Start game over (do not refresh page)
+
+    //cptr chooses new letter
+
+
+    //!!INCORRECT!!
+
+    //if letter is wrong
+
+    else if (computerInput != userGuesses) {
+
+        //# of guesses left decreases by 1 (update)
+        document.getElementById("guessesLeft").innerHTML = guessesLeft --;
+        //"guesses so far" are displayed... show what the user has entered (.innerHTML?)
+        document.getElementById("guesses").innerHTML = userGuesses;
+    }
+
+    var updateGuessesSoFar = function () {
+        // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+        document.querySelector('#guesses').innerHTML = "Your Guesses so far: " + guesses.join(', ');
+    };
+
+
+    //!!LOSSES
+
+    //if user guesses 9 times, then "Losses:" increase by 1 (update)
+
+    if (guessesLeft == 0) {
+        losses++;
+        document.getElementById("message").textContent = "Sorry, you lost. Better luck next time.";
+        document.getElementById("losses").textContent = losses;
+        guessesLeft = 11;
     }
 }
 
@@ -38,17 +83,4 @@ document.onkeyup = function (event) {
         //cptr chooses new letter
 
 
-//!!INCORRECT!!
 
-    //if letter is wrong 
-        //# of guesses left decreases by 1 (update)
-
-
-        //"guesses so far" are displayed... show what the user has entered (.innerHTML?)
-
-//!!LOSSES
-    //if user guesses 9 times, then "Losses:" increase by 1 (update)
-
-    //Start game over (do not refresh page)
-
-        //cptr chooses new letter
